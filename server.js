3832662fdab361ff87465180a4ec9a10544f3809
Manmeet567 <manmeet567singh@gmail.com>
2025-01-23 13:34:12 +0530

    Created server and connected with mongodb

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use(express.json());
+
+app.get("/test-server", (req, res) => {
+  res.send("Server is working");
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("connected to DB and server listening on port", PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error.message);
+  });
